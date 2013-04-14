62f4e80f659bcbf46f4e49d34f0bd1e450eae555
Vincent De Maertelaere <vincentdm@gmail.com>
2013-04-14 17:05:59 +0200

    fixed linking of friends

--- src/libjeugddag/InschrijvingsLijst.cpp
+++ src/libjeugddag/InschrijvingsLijst.cpp
@@ -45,29 +45,20 @@ void InschrijvingsLijst::ReadFromCSV(std::string filename) {
 		kind->voornaam=rij[CSV_INSCHRIJVING_VOORNAAM];
 		kind->adres=rij[CSV_INSCHRIJVING_ADRES];
 		kind->telefoon=rij[CSV_INSCHRIJVING_TELNUMMER];
-		//kind->gsm=rij[CSV_INSCHRIJVING_GSMNUMMER];
+		kind->gsm=rij[CSV_INSCHRIJVING_GSMNUMMER];
 		kind->geboortedatum=rij[CSV_INSCHRIJVING_GEBOORTEDATUM];
 		kind->opstapplaats=rij[CSV_INSCHRIJVING_OPSTAPPLAATS];
-		if(kind->geboortedatum.size()>2) {
-			kind->leeftijd=calculateAge(rij[CSV_INSCHRIJVING_GEBOORTEDATUM]);
-		} else {
-			kind->leeftijd=std::atoi(rij[CSV_INSCHRIJVING_GEBOORTEDATUM].c_str());
-		}
+		kind->leeftijd=calculateAge(rij[CSV_INSCHRIJVING_GEBOORTEDATUM]);
 
 		Inschrijving * inschrijving = new Inschrijving();
 		inschrijving->id=id;
 		inschrijving->kind=kind;
 		this->inschrijvingen.push_back(inschrijving);
 
-		int wsId=0;
-		for (wsId=0;wsId<CSV_INSCHRIJVING_WORKSHOP_COUNT;wsId++) {
+		for (int wsId=0;wsId<CSV_INSCHRIJVING_WORKSHOP_COUNT;wsId++) {
 			inschrijving->workshopnamen.push_back(rij[CSV_INSCHRIJVING_WORKSHOP_START+wsId]);
 		}
-		int wsrId = 0;
-
-		for (wsrId=0; wsrId < CSV_INSCHRIJVING_WORKSHOP_RES_COUNT;wsrId++) {
-			inschrijving->workshopnamen.push_back(rij[CSV_INSCHRIJVING_WORKSHOP_RES_START+wsrId]);
-		}
+		inschrijving->workshopnamen.push_back(rij[CSV_INSCHRIJVING_WORKSHOP_RES]);
 
 	}
 }
@@ -82,9 +73,9 @@ void InschrijvingsLijst::WriteToCSV(std::string filename) {
 		CSVFile::ROW row;
 		row.push_back(k->naam);
 		row.push_back(k->voornaam);
-		row.push_back(k->leeftijd);
-		row.push_back(k->opstapplaats);
+		row.push_back(k->geboortedatum);
 		row.push_back(k->telefoon);
+		row.push_back(k->gsm);
 
 		Kind::WorkshopSessionList_t::iterator wsIt;
 		for(wsIt=k->toegekendeWorkshops.begin();wsIt!=k->toegekendeWorkshops.end();wsIt++) {
@@ -134,6 +125,21 @@ void InschrijvingsLijst::Link(const WorkshopCollection& workshops) {
 					inschrijving->workshops.push_back(workshop);
 				}
 
+			}
+		}
+
+		InschrijvingsLijst_t::iterator vrIt;
+		inschrijving->vriendje = 0;
+		inschrijving->kind->vriendje=0;
+		for(vrIt=inschrijvingen.begin();
+				vrIt!=inschrijvingen.end() && inschrijving->vriendje==0;
+				vrIt++) {
+			Inschrijving * mVriendje = *vrIt;
+			std::string naam = mVriendje->kind->naam + " " + mVriendje->kind->voornaam;
+			std::string naam2= mVriendje->kind->voornaam + " " + mVriendje->kind->naam;
+			if(inschrijving->kind->naamVriendje == naam || inschrijving->kind->naamVriendje == naam2) {
+				inschrijving->kind->vriendje= mVriendje->kind;
+				inschrijving->vriendje=mVriendje;
 			}
 		}
 	}
